0483e4994d6db4db666628617211320562444a5e
Siyuan Liu <lsy997168748@gmail.com>
2021-01-15 09:11:07 -0800

    modify

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import requests
 print("version of requests: ",requests.__version__)
 
 google = requests.get('https://www.google.com')
-print(google.text)
+print(google.status_code)
 
 
 script= requests.get('https://raw.githubusercontent.com/lsykanade/cmput404lab1/main/script.py')
